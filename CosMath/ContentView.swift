751d9ceed77cae3b8662cb487ce466017958d707
rubythebelle <rubybellekim@gmail.com>
2024-10-13 12:39:17 -0700

    Initial Commit

---
+++ CosMath/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CosMath
+//
+//  Created by Ruby Kim on 2024-10-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CosMath/CosMathApp.swift
@@ -0,0 +1,17 @@
+//
+//  CosMathApp.swift
+//  CosMath
+//
+//  Created by Ruby Kim on 2024-10-13.
+//
+
+import SwiftUI
+
+@main
+struct CosMathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
